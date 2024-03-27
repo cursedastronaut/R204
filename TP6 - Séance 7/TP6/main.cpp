acd37e3d1e8cd9216567fc0c7f8597c175bd7018
Martineaux Galaad <galaad.martineaux@universite-paris-saclay.fr>
2024-03-27 09:22:19 +0100

    TP6: Projetx64: Ajout des commentaires

--- TP6 - Séance 7/TP6/main.cpp
+++ TP6 - Séance 7/TP6/main.cpp
@@ -1,3 +1,11 @@
+/*
+* Université Paris Saclay (C) - Galaad Martineaux
+* Le fichier main.cpp contient un programme principal C++ classique qui saisit 2 entiers puis appelle une fonction
+* somme qui va retourner comme résultat la somme de ces 2 entiers. La particularité de ce programme est que la
+* fonction somme n’est pas écrite dans le fichier main.cpp mais dans un autre fichier et dans un autre langage (ici
+* ASM x64) qui utilise une convention d’appel spécifique. C’est pourquoi il faut ajouter le qualificatif extern "C"
+* lors de la déclaration de la fonction. 
+*/
 #include <iostream>
 using namespace std;
 extern "C" int somme(int a, int b);
--- TP6 - Séance 7/TP6/prog.asm
+++ TP6 - Séance 7/TP6/prog.asm
@@ -1,3 +1,9 @@
+; Université Paris-Saclay (C) - IUT d'Orsay - Galaad Martineaux
+; Le fichier prog.asm contient lui la définition de la fonction somme en ASM x64. Lorsque le programme principal
+; C++ va appeler la fonction somme, il va placer le premier paramètre (l’entier a) dans le registre ECX et le
+; deuxième paramètre (l’entier b) dans le registre EDX et il récupérera la valeur de retour à partir du registre EAX.
+; C’est pourquoi la fonction somme copie d’abord ECX dans EAX, puis ajoute le contenu de EDX à EAX
+
 .CODE
 	somme PROC
 		MOV EAX,ECX
