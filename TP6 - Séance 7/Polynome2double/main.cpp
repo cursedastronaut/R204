87e1fe668b134f4febcf1dfbf73bdf1a43fd4491
Martineaux Galaad <galaad.martineaux@universite-paris-saclay.fr>
2024-03-27 10:06:09 +0100

    TP6: Ajout d'une ébauche de Polynome2double

---
+++ TP6 - Séance 7/Polynome2double/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+extern "C" double polynome2double(double a, double b, double c, double n);
+int main()
+{
+	double a, b, c, n;
+	cout << endl << "Ce programme sert a calculer la valeur d'un polynome entier de second degre a * n ^ 2 + b * n + c" << endl << endl;
+	cout << "Entrez une valeur decimale pour a : "; cin >> a;
+	cout << "Entrez une valeur decimale pour b : "; cin >> b;
+	cout << "Entrez une valeur decimale pour c : "; cin >> c;
+	cout << "Entrez une valeur decimale pour n : "; cin >> n;
+	cout << endl << "Pour n = " << n << " : " << a << "*n^2 + " << b << "*n + " << c
+		<< " = " << polynome2double(a, b, c, n) << endl;
+	return 0;
+}
---
+++ TP6 - Séance 7/Polynome2double/prog.asm
@@ -0,0 +1,38 @@
+.DATA
+	a REAL8 ?
+	b REAL8 ?
+	c REAL8 ?
+	n REAL8 ?
+	tmp REAL8 ?
+.CODE
+	polynome2double PROC
+		; la fonction appelante passe a dans XMM0, b dans XMM1,
+		; c dans XMM2 et n dans XMM3
+		MOVSD xmm0, a
+		MOVSD xmm1, b
+		MOVSD xmm2, c
+		MOVSD xmm3, n
+
+		; Calcul de n^2
+		MOVSD xmm4, xmm3
+		MULSD xmm4, xmm4 ; n^2
+
+		; Calcul de a*n^2
+		MOVSD xmm5, xmm0
+		MULSD xmm5, xmm4 ; a*n^2
+
+		; Calcul de b*n
+		MULSD xmm3, xmm1 ; b*n
+
+		; Calcul de a*n^2 + b*n
+		ADDSD xmm5, xmm3
+
+		; Calcul de a*n^2 + b*n + c
+		ADDSD xmm5, xmm2
+
+		MOVSD tmp, xmm5 ; Stockage du résultat temporaire
+		MOVSD xmm0, xmm5 ; Chargement du résultat dans xmm0 pour le retour
+
+		RET ; Le résultat à retourner est dans xmm0
+	polynome2double ENDP
+END
